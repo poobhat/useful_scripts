c3e86aa3d0e0c1dfca8d4448f8653f8c5e7204d9
Pooja Bhat <31859043+poobhat@users.noreply.github.com>
2020-05-28 16:15:42 +0530

    Simple python mail utility, with From, To and Subject prompts

---
+++ sendmail.py
@@ -0,0 +1,50 @@
+# --------------------------------------------------------------------------------------------
+# Generic python script for sending email over any SMTP host
+# Created by: Pooja Bhat
+# Date: 26-May-2020
+# --------------------------------------------------------------------------------------------
+
+import smtplib, email
+from email import encoders
+import os
+import sys
+
+SMTP_SERVER = '<smtp host ip address here>'
+SMTP_PORT = <smpt port>
+
+# SMTP_USERNAME = ''
+# SMTP_PASSWORD = ''
+
+print("To send an attachment execute: `python sendmail.py /path/to/your/attachment`\n\n")
+
+SMTP_FROM = raw_input("Enter FROM address: ")
+SMTP_TO = raw_input("Enter TO address: ")
+SUBJECT = raw_input("SUBJECT: ")
+
+text=""""""
+# MESSAGE = 'Subject: {}\n\n{}'.format(SUBJECT,text)
+MESSAGE = '{}\n'.format(text)
+
+msg = email.MIMEMultipart.MIMEMultipart()
+body = email.MIMEText.MIMEText(MESSAGE)
+
+if len(sys.argv)==2:
+	FILENAME = sys.argv[1]
+	attachment = email.MIMEBase.MIMEBase('text', 'plain')
+	attachment.set_payload(open(FILENAME).read())
+	attachment.add_header('Content-Disposition', 'attachment', filename=os.path.basename(FILENAME))
+	encoders.encode_base64(attachment)
+	msg.attach(attachment)
+
+msg.attach(body)
+msg.add_header('From', SMTP_FROM)
+msg.add_header('To', SMTP_TO)
+msg.add_header('Subject', SUBJECT)
+
+# Send message
+mailer = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+# mailer.connect()
+# mailer.login(SMTP_USERNAME, SMTP_PASSWORD)
+mailer.sendmail(SMTP_FROM, [SMTP_TO], msg.as_string())
+mailer.close()
+print("Message has been sent")
